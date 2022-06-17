2ded8842320cc2d82d60e0814a8ec74581ca3463
oliv10 <revi99@me.com>
2022-06-17 10:30:43 -0400

    Updated to use a real argument parser

--- epss.py
+++ epss.py
@@ -1,21 +1,28 @@
 #!/bin/python3
 
 from src import *
-import sys
 import os.path as p
 from alive_progress import alive_bar
 import csv
+import argparse
 
 if __name__ == "__main__":
-    try:
-        readFile = sys.argv[1]
-    except IndexError:
-        print("CVS File not included: ./epss.py <path_to_input_file>")
-        exit(1)
-    try:
-        writeFile = sys.argv[2]
-    except IndexError:
-        writeFile = sys.argv[1].split(".")[0]+"_out.csv"
+
+    # Args 
+    parser = argparse.ArgumentParser(description="./epss.py <input_file>")
+    parser.add_argument('readFile')
+    parser.add_argument('-w', '--writeFile')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    args = parser.parse_args()
+
+    # Set Read / Write Files
+    readFile = args.readFile
+    if args.writeFile == None:
+        writeFile = readFile.split(".")[0]+"_out.csv"
+    else:
+        writeFile = args.writeFile
+    
+    # Check if Write File Exists / Ask to overwrite...
     if (p.exists(writeFile)):
             while True:
                 rm = input("Do you want to overwrite '" + writeFile + "'? [yes/no]: ")
@@ -25,8 +32,10 @@ if __name__ == "__main__":
                 elif rm.lower() == "yes":
                     break
 
-    eps = epss(True)
+    eps = epss(args.verbose)
 
+    # Strange work around to count number of rows in csv file to not cause a crash.
+    # Causes crash whe using len(list(reader)). Unkown as to why...
     def getRows(file):
         rows = 0
         with open(file, "r") as f:
