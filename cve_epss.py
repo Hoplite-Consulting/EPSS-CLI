8a450bad6497ec70362bfebdfc27e18cccd301d5
oliv10 <revi99@me.com>
2022-06-21 18:37:01 +0000

    Support for multiple CVE Lookups in cve_epss.py

--- cve_epss.py
+++ cve_epss.py
@@ -5,23 +5,24 @@ from alive_progress import alive_bar
 import argparse
 
 def main(args):
-    with alive_bar(title="Getting EPSS Data...") as bar:
-        eps = epss_v2(args.verbose)
-        data = eps.get(args.cve.upper())
-        bar()
-    try:
-        print(data.cve.upper() + ": " + data.CVE)
-        print(data.epss.upper() + ": " + data.EPSS)
-        print(data.percentile.capitalize() + ": " + str(float(data.PERCENTILE)*100) + "%")
-        print(data.date.capitalize() + ": " + data.DATE)
-    except TypeError:
-        print("No CVE Data Found")
+    for i in args.cve[0]:
+        with alive_bar(title="Getting EPSS Data...") as bar:
+            eps = epss_v2(args.verbose)
+            data = eps.get(i.upper())
+            bar()
+        try:
+            print(data.cve.upper() + ": " + data.CVE)
+            print(data.epss.upper() + ": " + data.EPSS)
+            print(data.percentile.capitalize() + ": " + str(float(data.PERCENTILE)*100) + "%")
+            print(data.date.capitalize() + ": " + data.DATE)
+        except TypeError:
+            print("No CVE Data Found")
 
 if __name__ == "__main__":
 
     # Args 
     parser = argparse.ArgumentParser(description="./epss.py <cve>")
-    parser.add_argument('cve', help="CVE Number")
+    parser.add_argument('cve', action='append', nargs='*', help="CVE Number(s)")
     parser.add_argument('-v', '--verbose', action='store_true', help="Verbose Output")
     args = parser.parse_args()
 
