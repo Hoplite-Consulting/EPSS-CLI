23d48f7160b486310244bd5f684f42dbee8675d3
oliv10 <revi99@me.com>
2022-06-21 19:14:09 +0000

    Clean up Output in cve_epss.py

--- cve_epss.py
+++ cve_epss.py
@@ -1,22 +1,27 @@
 #!/bin/python3
 
 from src import *
-from alive_progress import alive_bar
+from alive_progress import alive_it
 import argparse
 
 def main(args):
-    for i in args.cve[0]:
-        with alive_bar(title="Getting EPSS Data...") as bar:
-            eps = epss_v2(args.verbose)
-            data = eps.get(i.upper())
-            bar()
+    bar = alive_it(args.cve[0], title="Getting EPSS Data...")
+    prev = ""
+    for i in bar:
+        eps = epss_v2(args.verbose)
+        data = eps.get(i.upper())
         try:
-            print(data.cve.upper() + ": " + data.CVE)
-            print(data.epss.upper() + ": " + data.EPSS)
-            print(data.percentile.capitalize() + ": " + str(float(data.PERCENTILE)*100) + "%")
-            print(data.date.capitalize() + ": " + data.DATE)
+            c = data.cve.upper() + ": " + data.CVE
+            e = data.epss.upper() + ": " + data.EPSS
+            p = data.percentile.capitalize() + ": " + str(float(data.PERCENTILE)*100) + "%"
+            d = data.date.capitalize() + ": " + data.DATE
+            print(c)
+            print(e)
+            print(p)
+            print(d)
         except TypeError:
             print("No CVE Data Found")
+        print("--------------------")
 
 if __name__ == "__main__":
 
