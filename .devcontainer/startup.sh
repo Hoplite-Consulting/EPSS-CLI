ee83aea827ef7d162ce556285c3ca082b01fd668
oliv10 <revi99@me.com>
2022-07-06 20:56:37 +0000

    Fix Devcontainer

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -3,8 +3,8 @@
 echo "Running Startup Script"
 
 # Python Shits
-pipreqs /workspaces/EPSS-API/ --force
-pip3 install -r /workspaces/EPSS-API/requirements.txt
+pipreqs --force
+pip3 install -r requirements.txt
 # rm /workspaces/EPSS-API/requirements.txt
 
 echo "Startup Script Complete"
